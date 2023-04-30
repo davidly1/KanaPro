1c601f0702366a34a12bab13c3027af07c956ec1
davidly <david26ly@gmail.com>
2023-04-29 21:13:03 -0400

    refactor input component code

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -11,10 +11,12 @@ const Input: React.FC<IInputProps> = ({ input, handleChange, isLight }) => {
       type='text'
       value={input}
       onChange={handleChange}
-      className={`block w-24 mx-auto pb-2 bg-transparent border-b-2 
-      ${isLight ? 'border-b-slate-800' : 'border-b-slate-200'} 
-      outline-none text-center text-6xl 
-      ${isLight ? 'text-slate-800' : 'text-slate-200'}`}
+      className={`block w-24 mx-auto pb-2 bg-transparent border-b-2 outline-none text-center text-6xl 
+      ${
+        isLight
+          ? 'border-b-slate-800 text-slate-800'
+          : 'border-b-slate-200 text-slate-200'
+      }`}
     />
   )
 }
